036b57b07f5e4d5ed2323ea9885e5e9d83ac3592
RubensRafael <rubensrafael2@live.com>
2021-11-01 17:25:49 -0200

    logout feito, falta reestruturar componenestes e comentar

--- polls-party/src/components/header.js
+++ polls-party/src/components/header.js
@@ -3,20 +3,30 @@ import '../style/header.css';
 import github from '../icons/github.svg'
 import logo from '../icons/logo.png'
 import logout from '../icons/logout.svg'
+import { withRouter } from 'react-router-dom';
 
+class Header extends React.Component {
+	constructor(props){
+		super(props);
+		this.handleLogout = this.handleLogout.bind(this);
+	}
 
-export default class Header extends React.Component {
-
+	handleLogout(){
+		localStorage.removeItem('token');
+		this.props.history.push({pathname:'/'})
+	}
 	render(){
 		return(
 			<header className="home-header">
 				<a href="https://github.com/RubensRafael/Polls-Party"><img className="header-icon" src={github} alt="github-repo"></img></a>
 				
 				<img src={logo} alt="logo"></img>
-				<img className="header-icon" src={logout} alt="logout"></img>
+				{localStorage.getItem('token') !== null ? <img onClick={this.handleLogout} className="header-icon" src={logout} alt="logout"></img>: <div></div>}
 			</header>
 
 			)
 
 	}
 }
+
+export default withRouter(Header)
